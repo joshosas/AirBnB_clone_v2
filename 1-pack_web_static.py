e065698915ac51044340752b41ded95de5d410ec
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2023-01-24 13:50:39 +0000

    correction 2.2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-A Fabric script that generates a .tgz archive from the contents of
-the web_static folder of the AirBnB Clone repo
+	A Fabric script that generates a .tgz archive from the contents of
+	the web_static folder of the AirBnB Clone repo
 """
 
 from fabric.api import *
@@ -10,15 +10,15 @@ from datetime import date
 
 
 def do_pack():
-"""
- Function makes a compressed archive file of a directory called "web_static"
- and saves it in a directory called "versions"
-"""
-    timestamp = time.strftime("%Y%m%d%H%M%S")
-    try:
-        local("mkdir -p versions")
-        local("tar -cvzf versions/web_static_{:s}.tgz web_static/".
-              format(timestamp))
-        return ("versions/web_static_{:s}.tgz".format(timestamp))
-    except Exception:
-        return None
+	"""
+	Function makes a compressed archive file of a directory called "web_static"
+	and saves it in a directory called "versions"
+	"""
+	timestamp = time.strftime("%Y%m%d%H%M%S")
+	try:
+		local("mkdir -p versions")
+		local("tar -cvzf versions/web_static_{:s}.tgz web_static/".
+              		format(timestamp))
+        	return ("versions/web_static_{:s}.tgz".format(timestamp))
+    	except Exception:
+        	return None
